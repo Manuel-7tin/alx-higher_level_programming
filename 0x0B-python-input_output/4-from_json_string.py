d7cfbd736241ea31dbe336c98f7ee2ae35d0825c
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-07-11 14:49:54 -0700

    Fifth taskgit push commit

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module shows the conversion of a strin to its object format using json
+"""
+import json
+
+
+
+def from_json_string(my_str):
+    """
+    This function converts a string to it's object format using json
+    """
+
+    return json.load(my_str)

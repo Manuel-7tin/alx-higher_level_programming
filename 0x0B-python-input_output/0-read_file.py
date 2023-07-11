d3d54283239fb24546b38621d9e21ac398f57229
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-07-11 08:55:05 -0700

    second option

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 This module  illustrates the act of reading frtom a file
 """
 
+
 def read_file(filename=""):
     """
     read_file - function: Takes in a file name and prints out the file content.

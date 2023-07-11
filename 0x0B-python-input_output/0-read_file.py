7063c218ad85eca8e3a8f5aaefadc17a0ad2cc0b
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-07-11 08:48:29 -0700

    firdt commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module  illustrates the act of reading frtom a file
+"""
+
+def read_file(filename=""):
+    """
+    read_file - function: Takes in a file name and prints out the file content.
+    """
+
+    with open(filename) as file:
+        print(file.read(), end="")

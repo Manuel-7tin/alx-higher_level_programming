4687064973078f38ca223fb2f14ebf100269ad42
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-07-12 07:26:07 -0700

    fifth task update

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -5,10 +5,9 @@ This module shows the conversion of a strin to its object format using json
 import json
 
 
-
 def from_json_string(my_str):
     """
     This function converts a string to it's object format using json
     """
 
-    return json.load(my_str)
+    return json.loads(my_str)
